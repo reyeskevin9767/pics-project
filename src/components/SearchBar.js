8c8cb82e2a00dba2e72d09b4b5b997be8bb39d3f
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-25 18:56:11 -0400

    add another event handler and use arrow functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,21 +2,34 @@ import React from 'react';
 
 // Class Funcition
 class SearchBar extends React.Component {
-  // Calls when someone changes the input
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
+  // Calls when someone changes the input
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  // Prevent the broswer from refreshing
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  // onChange and onSubmit -> Event Handlers
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             {/* Reference the method (callback Function) */}
             {/* Without (), function is called by input later */}
             {/* Automatically called with () */}
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              // Cotrolled Element - React stores data from DOM instead of getting it from DOM
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
