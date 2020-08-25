eb5044f6a8105680776ade666d949df22d345202
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-25 19:44:26 -0400

    move key to env file

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         query: term,
       },
       headers: {
-        Authorization: 'Client-ID ZCs-0-EIyFO_thcTrupXdmrC_4U3CZ79n5FBnlY35-I',
+        Authorization: process.env.REACT_APP_CLIENT_ID,
       },
     });
   }
