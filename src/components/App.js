1e24d4b6d9a6fd572edf42d2a32abdbc79aa18c7
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-25 19:26:16 -0400

    send request to api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 // Class Component
 class App extends React.Component {
+  // Send request to unsplash API
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: 'Client-ID ZCs-0-EIyFO_thcTrupXdmrC_4U3CZ79n5FBnlY35-I',
+      },
+    });
   }
 
   render() {
