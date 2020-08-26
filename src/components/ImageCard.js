10ac8baf957421de1f7d64a7a38f3e90d4277ac5
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-25 20:56:46 -0400

    create componentdidMount

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+// Class Component
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Direct access to a single DOM Element
+    // Assign to instance variable and pass to a JSX element as props
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // Get the image height after the page has load
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
+    const { description, urls } = this.props.image;
     return (
       <div>
-        <img
-          alt={this.props.image.description}
-          src={this.props.image.urls.regular}
-        />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
